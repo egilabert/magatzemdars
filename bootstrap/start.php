42748e2e58667bbd6ec428fe4b099130421f6c5d
Enric Gilabert <gilabert.enricggmail.com>
2014-12-14 21:56:13 +0100

    function environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function() {
 	
-	return 'production';//getenv('APP_ENV') ?: 'local';
+	return getenv('APP_ENV') ?: 'local';
 
 });
 
