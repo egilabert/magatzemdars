34e474c29db448de5d3fa3c077fd8e42ed12da2a
Enric Gilabert <gilabert.enricggmail.com>
2014-12-14 22:09:52 +0100

    global new environments

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-    'production' => array('95.85.20.197'),
+    'production' => array('magatzemdars'),
     'local' => array('homestead', '.local')
 ));
 
